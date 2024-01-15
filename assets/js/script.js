843dc5936647054ce72371f2ee68e8ceed44f796
Benjamin Schäfer <benibschafer@gmail.com>
2024-01-15 09:50:33 +0000

    feat: add createDiv function #5
    This will draw the gameboard. Each created div will have the correct
    width, height and colour to make up a piece of the gameboard.
    After each div is created it is appended to the div with
    id="gameboard"

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,40 @@ for (let i = 0; i <= size; i++) {
     }
 }
 
+/**
+ * Creates a div element with specified x and y values. 
+ * Depending on if the x and y values are even or odd, 
+ * the div will have a different color and size.
+ * 
+ * @param {number} x - The x value.
+ * @param {number} y - The y value.
+ * @returns {HTMLDivElement} - The created div element.
+ */
 function createDiv(x, y) {
+    let div = document.createElement('div');
+    div.xVal = x;
+    div.yVal = y;
+    div.innerHTML = "" + x + " / " + y;
 
+    if (x % 2 === 0 && y % 2 === 1) { // div is a horizontal border
+        div.style.backgroundColor = 'red';
+        div.style.width = '90px';
+        div.style.height = '30px';
+        return div;
+    } else if (x % 2 === 1 && y % 2 === 1) { // div is a cell
+        div.style.backgroundColor = 'blue';
+        div.style.width = '90px';
+        div.style.height = '90px';
+        return div;
+    } else if (x % 2 === 1 && y % 2 === 0) { // div is a vertical border
+        div.style.backgroundColor = 'red';
+        div.style.width = '30px';
+        div.style.height = '90px';
+        return div;
+    } else { // div is a corner
+        div.style.backgroundColor = 'gray';
+        div.style.width = '30px';
+        div.style.height = '30px';
+        return div;
+    }
 }
